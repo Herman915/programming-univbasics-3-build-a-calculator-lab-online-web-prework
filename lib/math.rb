3d88bac87938910d6d92e7689c73097edbfcef8f
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-05 22:37:45 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,35 +1,46 @@
 def addition(num1, num2)
-  num1 + num2
+  addition = num1 + num2
+  num1 = 5
+  num2 = 2
   puts addition
 
 end
 
 def subtraction(num1, num2)
   subtraction = num1 - num2
+  num1 = 5
+  num2 = 2
   puts subtraction
 
 end
 
 def division(num1, num2)
   division = num2 / num1
+  num1 = 5
+  num2 = 2
   puts division
 
 end
 
 def multiplication(num1, num2)
   multiplication = num1 * num2
+  num1 = 5
+  num2 = 2
   puts multiplication
 
 end
 
 def modulo(num1, num2)
-  modulo = num2 % num1 
+  modulo = num2 % num1
+  num1 = 5
+  num2 = 2
   puts modulo
 
 end
 
 def square_root(num)
   square_root = num **2
+  num = 5
   puts square_root
 
 end
