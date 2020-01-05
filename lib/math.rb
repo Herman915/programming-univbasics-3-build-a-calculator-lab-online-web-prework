befebf0827972ede51ef4dc3b3c223d5a71ad056
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-05 23:34:51 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,37 @@
+require "pry"
+
+
+
 def addition(num1, num2)
+#  binding.pry
+   num1 + num2
 
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 
 end
 
-def division(num1, num2)
+def division(num2, num1)
+  num2 / num1
+  
 
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 
 end
 
-def modulo(num1, num2)
+def modulo(num2, num1)
+  num2 % num1
+  
 
 end
 
 def square_root(num)
+  num //2
+
 
 end
