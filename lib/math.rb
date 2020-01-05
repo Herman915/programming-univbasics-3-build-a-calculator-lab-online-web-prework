f3f5f60a44f0c0315c7a52b4433e2bbcd159dcb6
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-05 21:57:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,14 +5,20 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  subtraction = num1 - num2
+  puts subtraction
 
 end
 
 def division(num1, num2)
+  division = num1 / num2
+  puts division
 
 end
 
 def multiplication(num1, num2)
+  multiplication = num1 * num2
+  puts multiplication
 
 end
 
